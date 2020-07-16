cee8ac090f5636c25174645a8b198503de5ecec3
Sakusaku39 <64998625+Sakusaku39@users.noreply.github.com>
2020-07-17 03:42:15 +0900

    Add files via upload

---
+++ 24.py
@@ -0,0 +1,18 @@
+#記事から参照されているメディアファイルをすべて抜き出せ．
+
+import read20
+import re
+
+text = read20.file_reading()
+text = text.split('\n')
+
+def mediafile(line):
+    deta = re.search(r'(?<=ファイル:)[^\|\]]+', line)
+    return deta.group()
+
+for line in text:
+    if re.search(r'ファイル:', line):
+        #print(line)
+        print(mediafile(line))
+
+#[^ ]括弧内に含まれない１文字
