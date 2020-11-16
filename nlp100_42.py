81c461c4d01c195fe27eff575bc812843bfc79bc
Sakusaku39 <64998625+Sakusaku39@users.noreply.github.com>
2020-11-17 04:20:09 +0900

    43,44　グラフあってるんかわからんしどうゆうことかもあんまりわかってない

--- nlp100_42.py
+++ nlp100_42.py
@@ -7,13 +7,19 @@
 #係り元と係り先：a modified source and a modifying destination
 import nlp100_41
 data = nlp100_41.C_contact();
-'''
+'''(41)
+source = []
+destination = []
+
 for i in range(3):
     for chunk in data[i]:
         if int(chunk.dst) != -1:
-            source = ''.join([morph.surface for morph in chunk.morphs])
-            destination = ''.join([morph.surface for morph in data[i][int(chunk.dst)].morphs])
-            print('{}->{}'.format(source, destination))
+            for morph_sou, morph_dst in zip(chunk.morphs, data[i][int(chunk.dst)].morphs):
+                source.append(morph_sou.surface)
+                destination.append(morph_dst.surface)
+            print('{}->{}'.format(''.join(source), ''.join(destination)))
+            source = []
+            destination = []
 '''
 
 for i in range(3):
@@ -23,3 +29,13 @@ for i in range(3):
         destination = ''.join([morph.surface if morph.pos != '記号' else '' for morph in data[i][int(chunk.dst)].morphs])
         #print('{}->{}'.format(source, destination))
         print(source, destination, sep='\t')
+
+'''リスト改めて内包表記の勉強
+
+odds = []
+for i in range(10):
+    if i % 2 == 1:
+        odds.append(i)
+↓
+odds = [i for i in range(10) if i % 2 == 1]
+'''
