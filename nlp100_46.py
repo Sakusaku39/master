1050af4f59161ca6590285990a58ce90aff74dde
wada <64998625+Sakusaku39@users.noreply.github.com>
2020-11-30 03:59:22 +0900

    46, 47とりあえず

--- nlp100_46.py
+++ nlp100_46.py
@@ -15,38 +15,28 @@ data = nlp100_41.C_contact()
 sources = dict()
 for i in range(len(data)):
     for chunk in data[i]:
-      for morph_d in chunk.morphs:
-        if morph_d.pos == '動詞': #動詞が入っている文節かどうかが知りたい
-            destination = morph_d.base  #動詞ならばmorphのベースを係り先の述語を入れておく変数に格納
-            '''
-            source_postpositional = []  #係り元の助詞のみのを入れておくリスト　これをキーにしたい
-            source_phrase = []          #係り元の文節を入れておくリスト    こっちは値にする
-            '''
-            sources.clear()
-            for src in chunk.srcs:  #係り先の番号のリストを回す
-                for morph_s in data[i][src].morphs:
-                    if morph_s.pos == '助詞': #助詞の入っている文節ならば
-                        '''
-                        source_postpositional.append(morph_s.surface)
-                        source_phrase.append(''.join([morph_s.surface if morph_s.pos != '記号' else '' for morph_s in data[i][src].morphs]))
-                        '''
-                        phrase = ''.join([morph_s.surface if morph_s.pos != '記号' else '' for morph_s in data[i][src].morphs])
-                        #sources[morph_s.surface] = phrase
-                        source = {morph_s.surface:phrase}
-                        sources.update(source)
-                        """
-                        sources.update(source)
-                        AttributeError: 'list' object has no attribute 'update'
-                        """
-            '''
-            source_postpositional.sort()
-            source_phrase.sort()
-            source_postpositional = ' '.join(source_postpositional)
-            source_phrase = ' '.join(source_phrase)
-            '''
-            sources = sorted(sources.items())
-            #key = ' '.join(sources.keys())
-            #value = ' '.join(sources.values())
-            #if sources != '':
-                #print(chunk.srcs)
-                #print('{}\t{}\t{}'.format(destination, key, value))
+        for morph_d in chunk.morphs:
+            if morph_d.pos == '動詞': #動詞が入っている文節かどうかが知りたい
+                destination = morph_d.base  #動詞ならばmorphのベースを係り先の述語を入れておく変数に格納
+                '''
+                source_postpositional = []  #係り元の助詞のみのを入れておくリスト　これをキーにしたい
+                source_phrase = []          #係り元の文節を入れておくリスト    こっちは値にする
+                '''
+                sources.clear()
+                for src in chunk.srcs:  #係り先の番号のリストを回す
+                    for morph_s in data[i][src].morphs:
+                        if morph_s.pos == '助詞': #助詞の入っている文節ならば
+                            phrase = ''.join([morph_s.surface if morph_s.pos != '記号' else '' for morph_s in data[i][src].morphs])
+                            #print(morph_s.surface)
+                            #print(phrase)
+                            sources[morph_s.surface] = phrase
+
+                s = sorted(sources.items()) #なんか中身がタプルになっちゃうから仕方なく
+                sources.clear()
+                sources.update(s)
+                #print(sources)
+                postpositional = ' '.join(sources.keys())
+                phrase = ' '.join(sources.values())
+                #postpositional = [num[0] for source in sources for num in source]
+                if sources != '':
+                    print('{}\t{}\t{}'.format(destination, postpositional, phrase))
